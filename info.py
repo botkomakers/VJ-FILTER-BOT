02b7a5113123432791080f48de14203dd91a2755
siam420borna <siam420borna@gmail.com>
2025-04-24 13:38:16 +0600

    Update info.py

--- info.py
+++ info.py
@@ -78,7 +78,34 @@ PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', True)) #
 REFERAL_COUNT = int(environ.get('REFERAL_COUNT', '20')) # number of referal count
 REFERAL_PREMEIUM_TIME = environ.get('REFERAL_PREMEIUM_TIME', '1month') # time in week, day, month.
 PAYMENT_QR = environ.get('PAYMENT_QR', 'https://envs.sh/C-l.jpg') # payment code picture url.
-PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟᴀɴs - \n\n- 0.10$ - 1 ᴡᴇᴇᴋ\n- 0.20$ - 1 ᴍᴏɴᴛʜs\n- 0.50$ - 3 ᴍᴏɴᴛʜs\n- 0.90$ - 6 ᴍᴏɴᴛʜs\n\n🎁 ᴘʀᴇᴍɪᴜᴍ ғᴇᴀᴛᴜʀᴇs 🎁\n\n○ ɴᴏ ɴᴇᴇᴅ ᴛᴏ ᴠᴇʀɪғʏ\n○ ɴᴏ ɴᴇᴇᴅ ᴛᴏ ᴏᴘᴇɴ ʟɪɴᴋ\n○ ᴅɪʀᴇᴄᴛ ғɪʟᴇs\n○ ᴀᴅ-ғʀᴇᴇ ᴇxᴘᴇʀɪᴇɴᴄᴇ\n○ ʜɪɢʜ-sᴘᴇᴇᴅ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ\n○ ᴍᴜʟᴛɪ-ᴘʟᴀʏᴇʀ sᴛʀᴇᴀᴍɪɴɢ ʟɪɴᴋs\n○ ᴜɴʟɪᴍɪᴛᴇᴅ ᴍᴏᴠɪᴇs & sᴇʀɪᴇs\n○ ꜰᴜʟʟ ᴀᴅᴍɪɴ sᴜᴘᴘᴏʀᴛ\n○ ʀᴇǫᴜᴇsᴛ ᴡɪʟʟ ʙᴇ ᴄᴏᴍᴘʟᴇᴛᴇᴅ ɪɴ 1ʜ ɪꜰ ᴀᴠᴀɪʟᴀʙʟᴇ\n\n✨ Binance UID - <code>1122*****</code>\n\nᴄʟɪᴄᴋ ᴛᴏ ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴀᴄᴛɪᴠᴇ ᴘʟᴀɴ /myplan\n\n💢 ᴍᴜsᴛ sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛ ᴀғᴛᴇʀ ᴘᴀʏᴍᴇɴᴛ\n\n‼️ ᴀғᴛᴇʀ sᴇɴᴅɪɴɢ ᴀ sᴄʀᴇᴇɴsʜᴏᴛ ᴘʟᴇᴀsᴇ ɢɪᴠᴇ ᴜs sᴏᴍᴇ ᴛɪᴍᴇ ᴛᴏ ᴀᴅᴅ ʏᴏᴜ ɪɴ ᴛʜᴇ ᴘʀᴇᴍɪᴜᴍ</b>')
+PAYMENT_TEXT = environ.get('PAYMENT_TEXT = """<b>✨ ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʀᴇᴍɪᴜᴍ ᴘʟᴀɴs ✨
+
+• 0.10$ – 𝟭 𝙬𝙚𝙚𝙠  
+• 0.20$ – 𝟭 𝙢𝙤𝙣𝙩𝙝  
+• 0.50$ – 𝟯 𝙢𝙤𝙣𝙩𝙝𝙨  
+• 0.90$ – 𝟲 𝙢𝙤𝙣𝙩𝙝𝙨  
+• 1.50$ – 𝙇𝙄𝙁𝙀𝙏𝙄𝙈𝙀
+
+🎁 ᴘʀᴇᴍɪᴜᴍ ғᴇᴀᴛᴜʀᴇs 🎁
+
+✓ 𝙉𝙤 𝙑𝙚𝙧𝙞𝙛𝙞𝙘𝙖𝙩𝙞𝙤𝙣 𝙍𝙚𝙦𝙪𝙞𝙧𝙚𝙙  
+✓ 𝘿𝙞𝙧𝙚𝙘𝙩 𝙁𝙞𝙡𝙚 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙𝙨  
+✓ 𝙉𝙤 𝘼𝙙𝙨 / 𝙉𝙤 𝙋𝙤𝙥𝙪𝙥𝙨  
+✓ 𝙎𝙪𝙥𝙚𝙧-𝙁𝙖𝙨𝙩 𝘿𝙤𝙬𝙣𝙡𝙤𝙖𝙙 𝙇𝙞𝙣𝙠𝙨  
+✓ 𝙃𝙞𝙜𝙝-𝙌𝙪𝙖𝙡𝙞𝙩𝙮 𝙎𝙩𝙧𝙚𝙖𝙢𝙞𝙣𝙜  
+✓ 𝙐𝙣𝙡𝙞𝙢𝙞𝙩𝙚𝙙 𝙁𝙞𝙡𝙚 𝘼𝙘𝙘𝙚𝙨𝙨  
+✓ 𝙁𝙪𝙡𝙡 𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙁𝙧𝙤𝙢 𝘼𝙙𝙢𝙞𝙣  
+✓ 𝙋𝙧𝙞𝙤𝙧𝙞𝙩𝙮 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙃𝙖𝙣𝙙𝙡𝙞𝙣𝙜  
+✓ 𝙎𝙥𝙚𝙘𝙞𝙖𝙡 𝙁𝙞𝙡𝙩𝙚𝙧 𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙄𝙣 𝙂𝙧𝙤𝙪𝙥𝙨  
+✓ 𝙍𝙚𝙛𝙚𝙧 𝙖𝙣𝙙 𝙀𝙖𝙧𝙣 – 𝙜𝙞𝙫𝙚 𝙖𝙘𝙘𝙚𝙨𝙨, 𝙜𝙚𝙩 𝙚𝙭𝙩𝙧𝙖 𝙢𝙤𝙣𝙩𝙝𝙨
+
+💳 𝙋𝘼𝙔 𝙑𝙄𝘼 Binance UID: <code>1122*****</code>  
+🚀 𝙁𝙖𝙨𝙩 𝙖𝙘𝙩𝙞𝙫𝙖𝙩𝙞𝙤𝙣 – 𝙪𝙨𝙪𝙖𝙡𝙡𝙮 𝙬𝙞𝙩𝙝𝙞𝙣 5 𝙢𝙞𝙣𝙨
+
+📌 𝘾𝙝𝙚𝙘𝙠 𝙮𝙤𝙪𝙧 𝙘𝙪𝙧𝙧𝙚𝙣𝙩 𝙥𝙡𝙖𝙣: <code>/myplan</code>
+
+⚠️ 𝙎𝙚𝙣𝙙 𝙎𝙘𝙧𝙚𝙚𝙣𝙨𝙝𝙤𝙩 𝙖𝙛𝙩𝙚𝙧 𝙥𝙖𝙮𝙢𝙚𝙣𝙩  
+⏳ 𝙂𝙞𝙫𝙚 𝙪𝙨 𝙖 𝙢𝙤𝙢𝙚𝙣𝙩 𝙩𝙤 𝙖𝙙𝙙 𝙮𝙤𝙪 𝙞𝙣 𝙋𝙍𝙀𝙈𝙄𝙐𝙈</b>"""')
 
 
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
