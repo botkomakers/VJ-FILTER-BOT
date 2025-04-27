65b376c4087bdb71107e2a02d4a36cd15bf9733c
siam420borna <siam420borna@gmail.com>
2025-04-27 13:34:35 +0600

    Update info.py

--- info.py
+++ info.py
@@ -20,8 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 import os
 
 class Info:
-    TMDB_API_KEY = os.environ.get("c3443ed2f96cd615e3badf6b68c8a689", "তোমার টিএমডিবি এপিআই কি")
-
+    TMDB_API_KEY = os.environ.get("TMDB_API_KEY", "c3443ed2f96cd615e3badf6b68c8a689")
 
 
 #For Request optional
